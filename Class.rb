def48421cb4149ae9c3928a2283bdf59daf164b6
joem-asp20 <71202492+joem-asp20@users.noreply.github.com>
2020-09-18 15:17:14 +0900

    Update Class.rb

--- Class.rb
+++ Class.rb
@@ -1,11 +1,26 @@
 class Communication
-  
-  def gerrt
-   reply = "Hello"
-   
+  def greet
+    reply = gets.chomp
     if reply == "Hello"
-      puts  "(o^□^o)Hello"
-    else
-      puts "(･ω･｀)彡ﾌﾟｨ"
+        puts "Hello"
+    else 
+        puts "....."
+    end
+  end
 end
+greeting = Communication.new
+greeting.greet
 
+class WorkplaceCommunication < Communication
+  def greeting
+    puts "ここはあなたの職場ですか？はい/いいえ"
+      reply = gets.chomp
+     if reply == "はい"
+       puts "おはようございます"
+     else
+       puts "....."
+     end
+  end
+end
+Working = WorkplaceCommunication.new
+Working.greeting
